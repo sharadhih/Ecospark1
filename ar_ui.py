e36705efb15a8c12ab9dd7a3830cd3b74e1af053
Sharadhi Hegde <sharadhih00@gmail.com>
2024-05-18 16:16:30 +0530

    updated ar_ui

--- ar_ui.py
+++ ar_ui.py
@@ -15,38 +15,44 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure(1, weight=1)
 
         # load images with light and dark mode image
-        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "./assets")
+        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "assets\\")
         self.logo_image = tk.PhotoImage(file=os.path.join(image_path, "download.png"))
-        self.large_test_image = tk.PhotoImage(file=os.path.join(image_path, "download.png"))
-        self.image_icon_image = tk.PhotoImage(file=os.path.join(image_path, "download.png"))
+        self.large_test_image = tk.PhotoImage(file=os.path.join(image_path, "mini_moss.png"))
+        #self.large_test_image= self.large_test_image.subsample()
+        self.image_icon_image = tk.PhotoImage(file=os.path.join(image_path, "water_tap.png"))
 
-        self.home_image_light = tk.PhotoImage(file=os.path.join(image_path, "download.png"))
-        self.home_image_dark = tk.PhotoImage(file=os.path.join(image_path, "download.png"))
-        self.chat_image_light = tk.PhotoImage(file=os.path.join(image_path, "download.png"))
-        self.chat_image_dark = tk.PhotoImage(file=os.path.join(image_path, "download.png"))
-        self.add_user_image_light = tk.PhotoImage(file=os.path.join(image_path, "download.png"))
-        self.add_user_image_dark = tk.PhotoImage(file=os.path.join(image_path, "download.png"))
-
+        self.home_image_light = tk.PhotoImage(file=os.path.join(image_path, "search.png"))
+        self.home_image_light = self.home_image_light.subsample(16,12)
+        self.home_image_dark = tk.PhotoImage(file=os.path.join(image_path, "search.png"))
+        self.home_image_dark = self.home_image_dark.subsample(16,12)
+        self.chat_image_light = tk.PhotoImage(file=os.path.join(image_path, "sofa.png"))
+        self.chat_image_light = self.chat_image_light.subsample(16,12)
+        self.chat_image_dark = tk.PhotoImage(file=os.path.join(image_path, "sofa.png"))
+        self.chat_image_dark = self.chat_image_dark.subsample(16,12)
+        self.add_user_image_light = tk.PhotoImage(file=os.path.join(image_path, "recycle.png"))
+        self.add_user_image_light = self.add_user_image_light.subsample(16,12)
+        self.add_user_image_dark = tk.PhotoImage(file=os.path.join(image_path, "recycle.png"))
+        self.add_user_image_dark = self.add_user_image_dark.subsample(16,12)
         # create navigation frame
         self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
         self.navigation_frame.grid(row=0, column=0, sticky="nsew")
         self.navigation_frame.grid_rowconfigure(4, weight=1)
 
-        self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="  Image Example", image=self.logo_image,
+        self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="  EcoSpark", image=self.logo_image,
                                                              compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
         self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
 
-        self.home_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Home",
+        self.home_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Eco Scan",
                                                    fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
                                                    image=self.home_image_light, anchor="w", command=self.home_button_event)
         self.home_button.grid(row=1, column=0, sticky="ew")
 
-        self.frame_2_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Frame 2",
+        self.frame_2_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Green Furniture",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
                                                       image=self.chat_image_light, anchor="w", command=self.frame_2_button_event)
         self.frame_2_button.grid(row=2, column=0, sticky="ew")
 
-        self.frame_3_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Frame 3",
+        self.frame_3_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="RRR",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
                                                       image=self.add_user_image_light, anchor="w", command=self.frame_3_button_event)
         self.frame_3_button.grid(row=3, column=0, sticky="ew")
@@ -56,7 +62,7 @@ class App(customtkinter.CTk):
         self.appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
 
         # create home frame
-        self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="white")
         self.home_frame.grid_columnconfigure(0, weight=1)
 
         self.home_frame_large_image_label = customtkinter.CTkLabel(self.home_frame, text="", image=self.large_test_image)
